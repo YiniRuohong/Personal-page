7f2aebe6beea500602bf0caa854ca9682566fcf7
yiniruohong <14525324@qq.com>
2023-12-31 19:07:50 +0800

    details changed

--- js/common.js
+++ js/common.js
@@ -73,3 +73,20 @@ if ($ === undefined) {
 		}
 	})
 }
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = '(ฅ>ω<*ฅ) 我藏';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '被你发现啦~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
